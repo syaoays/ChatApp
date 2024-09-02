f1ace78f49111bc8ed6c9e1a896bc9b8aa0c6144
shunyu.yao <yaosys@outlook.com>
2024-09-02 16:29:35 -0700

    temp

---
+++ app/src/main/java/com/example/whatsapp/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.whatsapp.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.whatsapp.R
+
+class MainActivity : AppCompatActivity() {
+    private val binding:
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
